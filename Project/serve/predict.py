164ef6e6ee317c56cb3a7011c37fefa04d3e08e5
EC2 Default User <ec2-user@ip-172-16-84-4.us-east-2.compute.internal>
2020-02-19 06:06:13 +0000

    [UPDATE] Adding in code for predict.py, and train.py, and the project notebook, more to come.

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,12 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    
+    # Turn review into words.
+    review_words = review_to_words(input_data)
 
-    data_X = None
-    data_len = None
+    # Convert review into integer sequence.
+    data_X, data_len = convert_and_pad(model.word_dict, review_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +90,14 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    # Set all grad flags to false.
+    with torch.no_grad():
+        output = model.forward(data)
+        
+    # To numpy.
+    output = output.numpy()
+    
+    # Round to get result.
+    result = np.round(output)
 
-    return result
+    return int(result)
